c2bdc4be28084d4f98ddb29cc4c7934a600c25ee
GustavoAkyama <gustavoakyamamga01@gmail.com>
2023-03-13 18:02:52 -0300

    Project created

---
+++ src/main/java/com/akyama/sessao_23/Sessao23Application.java
@@ -0,0 +1,13 @@
+package com.akyama.sessao_23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sessao23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sessao23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/akyama/sessao_23/Sessao23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akyama.sessao_23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sessao23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
