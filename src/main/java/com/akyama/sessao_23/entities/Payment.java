5b2d5d86f860994550597904102383bafdc89577
GustavoAkyama <gustavoakyamamga01@gmail.com>
2023-03-14 20:37:55 -0300

    Subtotal & Total methods

--- src/main/java/com/akyama/sessao_23/entities/Payment.java
+++ src/main/java/com/akyama/sessao_23/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.akyama.sessao_23.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
