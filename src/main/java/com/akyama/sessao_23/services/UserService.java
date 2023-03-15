db9b7e8fb5eba89c7d518f70e11e45ab11561aaf
GustavoAkyama <gustavoakyamamga01@gmail.com>
2023-03-14 21:38:43 -0300

    Exception handling - update

--- src/main/java/com/akyama/sessao_23/services/UserService.java
+++ src/main/java/com/akyama/sessao_23/services/UserService.java
@@ -5,6 +5,7 @@ import com.akyama.sessao_23.entities.User;
 import com.akyama.sessao_23.repositories.UserRepository;
 import com.akyama.sessao_23.services.exceptions.DatabaseException;
 import com.akyama.sessao_23.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +46,14 @@ public class UserService {
     }
     
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
